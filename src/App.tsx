68d404b2ee162ef19b6956e11aa5c365ca5c186d
chi-wei-lien <lien2@purdue.edu>
2022-12-18 16:42:55 -0500

    complete google login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import handleLogin from "./utils/handleLogin";
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <GoogleLogin
+        onSuccess={handleLogin}
+        onError={() => {
+          console.error("Login Failed");
+        }}
+      />
     </div>
   );
 }
---
+++ src/utils/handleLogin.ts
@@ -0,0 +1,52 @@
+import { CredentialResponse } from "@react-oauth/google";
+import {
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+} from "react-google-login";
+
+const handleLogin = async (credentialResponse: CredentialResponse) => {
+  console.log(credentialResponse);
+  const res = await fetch("http://localhost:8080/api/auth/google", {
+    method: "POST",
+    body: JSON.stringify({
+      token: credentialResponse.credential,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+// const handleLogin = async (
+//   response: GoogleLoginResponse | GoogleLoginResponseOffline
+// ) => {
+//   console.log("called");
+//   if (!isGoogleLoginResponse(response)) {
+//     console.log("rejected");
+//     console.log(response);
+//     return;
+//   }
+//   console.log("fetched");
+//   const res = await fetch("http://localhost:8080/api/auth/google", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       token: response.tokenId,
+//     }),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+//   const data = await res.json();
+// };
+
+// const isGoogleLoginResponse = (
+//   response: GoogleLoginResponse | GoogleLoginResponseOffline
+// ): response is GoogleLoginResponse => {
+//   return (
+//     !!response &&
+//     typeof response === "object" &&
+//     !!(response as GoogleLoginResponse).tokenObj
+//   );
+// };
+
+export default handleLogin;
